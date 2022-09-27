c206d8261747268ae8f7685c4d090761cad5d56e
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2022-09-27 09:53:28 +0400

    Added static protocol stats box and FXD logo

--- fathom/src/components/Dashboard/ProtocolStats.tsx
+++ fathom/src/components/Dashboard/ProtocolStats.tsx
@@ -1,6 +1,7 @@
-import { Paper } from '@mui/material';
+import { Box, CardMedia, Grid, Paper } from '@mui/material';
 // import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import logo from '../../assets/images/fxd-logo.jpg';
 
 
 const ProtocolStats = function ProtocolStats(props: any) {
@@ -13,9 +14,17 @@ const ProtocolStats = function ProtocolStats(props: any) {
           height: 240,
         }}
       >
-      <Typography variant="h6" gutterBottom>
+      {/* <Typography variant="h6" gutterBottom>
           FXD
-      </Typography>
+      </Typography> */}
+      {/* <CardMedia
+        component="img"
+        width="80"
+        height="40"
+        image={logo}
+        alt="Fathom Stablecoin"
+      /> */}
+      <img src={logo} alt="Fathom Stablecoin"  height="40" width="80"></img>
       <Typography variant="subtitle1" gutterBottom>
           The reliable stablecoin that earns you extra passive income
       </Typography>
@@ -23,6 +32,85 @@ const ProtocolStats = function ProtocolStats(props: any) {
             FXD is an auto-farming stablecoin that earns passive yields for you in the background.
             Now, instead of paying for loans,you can get loans while earning on your collateral.
       </Typography>
+      <Grid container>
+      <Box
+        sx={{
+          boxShadow: 2,
+          width: '7rem',
+          height: '4rem',
+          bgcolor: '#F5F5F5',
+          p: 1,
+          m: 1,
+          borderRadius: 2,
+          textAlign: 'center',
+        }}
+      >
+       <Typography variant="subtitle1" color="text.secondary">
+          Total Supply
+       </Typography>
+       <Typography variant="body2" gutterBottom>
+          1000000000
+       </Typography>
+      </Box>
+      <Box
+        sx={{
+          boxShadow: 2,
+          width: '7rem',
+          height: '4rem',
+          bgcolor: '#F5F5F5',
+          p: 1,
+          m: 1,
+          borderRadius: 2,
+          textAlign: 'center',
+        }}
+      >
+      <Typography variant="subtitle1" color="text.secondary">
+          TVL
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+          1000000000
+      </Typography>
+      </Box>
+      <Box
+        sx={{
+          boxShadow: 2,
+          width: '7rem',
+          height: '4rem',
+          bgcolor: '#F5F5F5',
+          p: 1,
+          m: 1,
+          borderRadius: 2,
+          textAlign: 'center',
+        }}
+      >
+      <Typography variant="subtitle1" color="text.secondary">
+          FXD Price
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+          $ 1.00
+      </Typography>
+      </Box>
+      <Box
+        sx={{
+          boxShadow: 2,
+          width: '7rem',
+          height: '4rem',
+          bgcolor: '#F5F5F5',
+          p: 1,
+          m: 1,
+          borderRadius: 2,
+          textAlign: 'center',
+        }}
+      >
+      <Typography variant="subtitle1" color="text.secondary">
+          Stability Fee
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+          5%
+      </Typography>
+      </Box>
+    </Grid>
+
       </Paper>
 
     );
