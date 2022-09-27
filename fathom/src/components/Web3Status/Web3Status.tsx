1815af1ab5e26ae03c6d2c1281588d8ed454ce76
babchenko <vetalion1@gmail.com>
2022-09-27 14:19:23 +0300

    typescript fixes

--- fathom/src/components/Web3Status/Web3Status.tsx
+++ fathom/src/components/Web3Status/Web3Status.tsx
@@ -2,13 +2,18 @@ import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core";
 import { Chip } from "@mui/material";
 
 export const Web3Status = () => {
-  const { error } = useWeb3React();
+  const { error, account } = useWeb3React();
+
   if (error) {
     return (
       <Chip
         color="error"
         label={
-          error instanceof UnsupportedChainIdError ? "Wrong Network" : "Error"
+          error instanceof UnsupportedChainIdError
+            ? "Wrong Network"
+            : !account
+            ? "Wallet Request Permissions Error"
+            : "Error"
         }
       ></Chip>
     );
