5de2ab21a92402f9268cccce12cd715905fdc7c2
ztshort <ztshort@hotmail.com>
2022-09-29 15:38:27 -0500

    Added auto calculate fathom out token. Added safe max button

--- fathom/src/services/interfaces/IPoolService.ts
+++ fathom/src/services/interfaces/IPoolService.ts
@@ -3,4 +3,5 @@ import ICollatralPool from "../../stores/interfaces/ICollatralPool";
 export default interface IPoolService{
     fetchPools(): Promise<ICollatralPool[]>;
     getPriceWithSafetyMargin(pool:ICollatralPool): Promise<number>;
+    getUserTokenBalance(address:string, pool:ICollatralPool): Promise<number>;
 }
