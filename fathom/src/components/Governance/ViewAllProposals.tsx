0780dbbaeba1c7f1f44f46b25395296cf136918f
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2022-09-29 13:13:48 +0400

    tested and merged code from max for governance

--- fathom/src/components/Governance/ViewAllProposals.tsx
+++ fathom/src/components/Governance/ViewAllProposals.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import {
   Table,
   TableBody,
@@ -8,11 +7,11 @@ import {
   TableRow,
   Box,
   Container,
-  Link,
   Paper,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { Link } from 'react-router-dom';
 import { useStores } from "../../stores";
 import useMetaMask from "../../hooks/metamask";
 import { LogLevel, useLogger } from "../../helpers/Logger";
@@ -29,7 +28,7 @@ const AllProposalsView = observer(() => {
 
   useEffect(() => {
     logger.log(LogLevel.info, "fetching proposal information.");
-    proposeStore.fetchProposals(account); // where is this being used?
+    proposeStore.fetchProposals(account); 
   }, [account, logger, proposeStore]);
 
   return (
@@ -51,34 +50,37 @@ const AllProposalsView = observer(() => {
             Proposals
           </Typography>
           {proposeStore.fetchedProposals.length === 0 ? (
-            <Typography variant="h6">No proposals available</Typography>
+            <Typography variant='h6'>Loading all proposals</Typography>
           ) : (
-            <TableContainer>
+
+            <TableContainer >
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                   <TableRow>
                     <TableCell>Proposal Id Hash:</TableCell>
-                    <TableCell>Description:</TableCell>
+                    <TableCell>Title:</TableCell>
+                    <TableCell>Status:</TableCell>
                     <TableCell align="right"></TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {proposeStore.fetchedProposals.map((proposal: IProposal) => (
+                  {proposeStore.fetchedProposals.map((proposal:IProposal) => (
                     <TableRow
                       key={proposal.proposalId}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
                       <TableCell component="th" scope="row">
-                        <Link href={`/proposal/${proposal.proposalId}`}>
-                          {proposal.proposalId.substring(0, 4) +
-                            " ... " +
-                            proposal.proposalId.slice(-4)}
-                        </Link>
+                          <Link to={`/proposal/${proposal.proposalId}`} >{proposal.proposalId.substring(0,4) +" ... " +proposal.proposalId.slice(-4)}</Link>
+                      </TableCell>
+                    
+                      <TableCell component="th" scope="row">
+                        {proposal.description.split('----------------')[0].substring(0,50) +" ... "}
+                      </TableCell>
+                    
+                      <TableCell component="th" scope="row">
+                        {proposal.status}
                       </TableCell>
 
-                      <TableCell component="th" scope="row">
-                        {proposal.description.substring(0, 50) + " ... "}
-                      </TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
