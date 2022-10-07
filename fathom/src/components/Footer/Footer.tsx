47498c8d4d72e70817672f01caae6c33d07b0257
babchenko <vetalion1@gmail.com>
2022-10-07 15:08:49 +0300

    design changes

--- fathom/src/components/Footer/Footer.tsx
+++ fathom/src/components/Footer/Footer.tsx
@@ -1,18 +1,26 @@
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 
+const CopyrightStyles = {
+  position: "fixed",
+  bottom: "0",
+  width: '100%',
+  my: 4,
+  ml: "-100px"
+}
+
 
 const Copyright = function Copyright(props: any) {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyright © '}
-        <Link color="inherit" href="https://fathom.com/">
-          fathom.com
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props} sx={CopyrightStyles}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://fathom.com/">
+        Fathom App
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-  export default Copyright;
+export default Copyright;
