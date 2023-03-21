d0fe88daa100da18bbd56c56538d57ed5727566d
babchenko <vetalion1@gmail.com>
2023-03-21 11:06:10 +0300

    Open position validators

--- fathom/src/components/Positions/OpenPosition/OpenPositionForm.tsx
+++ fathom/src/components/Positions/OpenPosition/OpenPositionForm.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import BigNumber from "bignumber.js";
 import {
   Box,
   CircularProgress,
@@ -14,7 +17,6 @@ import {
   Summary,
   WalletBalance,
 } from "components/AppComponents/AppBox/AppBox";
-import { Controller } from "react-hook-form";
 import {
   AppFormInputLogo,
   AppFormInputWrapper,
@@ -30,9 +32,9 @@ import {
   ButtonsWrapper,
   MaxButton,
 } from "components/AppComponents/AppButton/AppButton";
-import React from "react";
 import useOpenPositionContext from "context/openPosition";
 import { styled } from "@mui/material/styles";
+import { FXD_MINIMUM_BORROW_AMOUNT } from "helpers/Constants";
 
 const OpenPositionFormWrapper = styled(Grid)`
   padding-left: 20px;
@@ -62,14 +64,11 @@ const OpenPositionForm = () => {
     fathomToken,
     safeMax,
     openPositionLoading,
-
     setMax,
     setSafeMax,
     onSubmit,
-
     control,
     handleSubmit,
-
     availableFathomInPool,
     onClose,
     pool,
@@ -93,16 +92,15 @@ const OpenPositionForm = () => {
           name="collateral"
           rules={{
             required: true,
-            min: 10,
-            max: +balance / 10 ** 18,
-            pattern: /^[1-9][0-9]*0$/,
+            min: 1,
+            max: BigNumber(balance).dividedBy(10 ** 18).toString(),
           }}
           render={({ field: { onChange, value }, fieldState: { error } }) => (
             <AppFormInputWrapper>
               <AppFormLabel>Collateral</AppFormLabel>
               {balance ? (
                 <WalletBalance>
-                  Wallet Available: {+balance / 10 ** 18} {pool.poolName}
+                  Wallet Available: {BigNumber(balance).dividedBy(10 ** 18).toString()} {pool.poolName}
                 </WalletBalance>
               ) : null}
               <AppTextField
@@ -111,18 +109,6 @@ const OpenPositionForm = () => {
                 placeholder={"0"}
                 helperText={
                   <>
-                    {error && error.type === "pattern" && (
-                      <>
-                        <InfoIcon sx={{ float: "left", fontSize: "18px" }} />
-                        <Box
-                          component={"span"}
-                          sx={{ fontSize: "12px", paddingLeft: "6px" }}
-                        >
-                          Allowed staked collateral should be multiple of 10
-                        </Box>
-                      </>
-                    )}
-
                     {error && error.type === "max" && (
                       <>
                         <InfoIcon sx={{ float: "left", fontSize: "18px" }} />
@@ -152,7 +138,7 @@ const OpenPositionForm = () => {
                           component={"span"}
                           sx={{ fontSize: "12px", paddingLeft: "6px" }}
                         >
-                          Minimum collateral amount is 10.
+                          Minimum collateral amount is 1.
                         </Box>
                       </>
                     )}
@@ -178,6 +164,7 @@ const OpenPositionForm = () => {
           name="fathomToken"
           rules={{
             required: true,
+            min: FXD_MINIMUM_BORROW_AMOUNT,
             validate: (value) => {
               if (Number(value) > availableFathomInPool) {
                 return "Not enough FXD in pool";
