a6d5c40085e26fd902c2e3faa8e3ae88bdb514d5
ztshort <ztshort@hotmail.com>
2023-04-28 21:01:10 -0500

    set xinfin network 50 as the default chain

--- fathom/src/helpers/Constants.ts
+++ fathom/src/helpers/Constants.ts
@@ -2,7 +2,7 @@ import BigNumber from "bignumber.js";
 
 export const XDC_BLOCK_TIME = 2; // 2 seconds
 export const ESTIMATE_GAS_MULTIPLIER = 1.2;
-export const DEFAULT_CHAIN_ID = 51;
+export const DEFAULT_CHAIN_ID = 50;
 
 export const YEAR_IN_SECONDS = 365 * 24 * 60 * 60
 export const DAY_IN_SECONDS = 24 * 60 * 60
@@ -28,7 +28,7 @@ export class Constants {
   public static WeiPerRad = new BigNumber("1e45");
   public static ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
   public static TransactionCheckUpdateInterval = 2000;
-  public static DEFAULT_CHAIN_ID = 51;
+  public static DEFAULT_CHAIN_ID = 50;
   public static MAX_UINT256 =
     "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 
