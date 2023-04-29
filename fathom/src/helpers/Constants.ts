1ad28f7960943c57f1db025ccd6c640e8d26c40d
ztshort <ztshort@hotmail.com>
2023-04-29 16:52:13 -0500

    decrease FXD min borrowed from 100 to 50

--- fathom/src/helpers/Constants.ts
+++ fathom/src/helpers/Constants.ts
@@ -7,7 +7,7 @@ export const DEFAULT_CHAIN_ID = 50;
 export const YEAR_IN_SECONDS = 365 * 24 * 60 * 60
 export const DAY_IN_SECONDS = 24 * 60 * 60
 
-export const FXD_MINIMUM_BORROW_AMOUNT = 100;
+export const FXD_MINIMUM_BORROW_AMOUNT = 50;
 
 export const CRYPTOCOMPARE_ENDPOINT = 'https://min-api.cryptocompare.com/data/price';
 
