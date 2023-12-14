867d4ea9cc023fe603bc72a9268f97bb6e467af5
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2023-12-14 14:49:38 +0400

    Added vaults project in playwright, added locators to vault page, vault data and one test for vaults

--- playwright-tests/pages/vault.page.ts
+++ playwright-tests/pages/vault.page.ts
@@ -4,16 +4,22 @@ import BasePage from "./base.page";
 export default class VaultPage extends BasePage {
   readonly path: string;
   readonly btnManageVault: Locator;
+  readonly btnDeposit: Locator;
 
   constructor(page: Page) {
     super(page);
     this.path = "/#/vault";
-
-    // Locators
-    this.btnManageVault = this.page.getByText("Manage Vault");
   }
 
   async navigate(): Promise<void> {
     await super.navigate(this.path);
+  }
+
+  getVaultRowLocatorByName(name: string): Locator {
+    return this.page.locator(`//div[contains(text(), "${name}")]/ancestor::tr`);
+  }
+
+  getActionButtonLocatorForVault(name: string): Locator {
+    return this.getVaultRowLocatorByName(name).locator("//td[last()]/button");
   }
 }
