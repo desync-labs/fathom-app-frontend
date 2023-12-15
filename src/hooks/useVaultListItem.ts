ccaa259613f62dd69d2f6f181c4a69c1f880545e
Oleksandr Dovgopoliy <alexdovgopoly@gmail.com>
2023-12-15 15:55:24 +0200

    Fix empty position balance validation

--- src/hooks/useVaultListItem.ts
+++ src/hooks/useVaultListItem.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { IVaultPosition } from "fathom-sdk";
+import BigNumber from "bignumber.js";
 
 interface UseVaultListItemProps {
   vaultPosition: IVaultPosition | null | undefined;
@@ -16,13 +17,16 @@ const useVaultListItem = ({ vaultPosition }: UseVaultListItemProps) => {
   const [manageVault, setManageVault] = useState<boolean>(false);
   const [newVaultDeposit, setNewVaultDeposit] = useState<boolean>(false);
   const [activeVaultInfoTab, setActiveVaultInfoTab] = useState<VaultInfoTabs>(
-    vaultPosition && vaultPosition.balanceShares !== "0"
+    vaultPosition && BigNumber(vaultPosition.balanceShares).isGreaterThan(0)
       ? VaultInfoTabs.POSITION
       : VaultInfoTabs.ABOUT
   );
 
   useEffect(() => {
-    if (vaultPosition && vaultPosition.balanceShares !== "0") {
+    if (
+      vaultPosition &&
+      BigNumber(vaultPosition.balanceShares).isGreaterThan(0)
+    ) {
       setActiveVaultInfoTab(VaultInfoTabs.POSITION);
     } else if (activeVaultInfoTab === VaultInfoTabs.POSITION) {
       setActiveVaultInfoTab(VaultInfoTabs.ABOUT);
