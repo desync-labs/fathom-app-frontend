8046b052f163104e01e396ef4bfeece0d722a2f3
babchenko <vetalion1@gmail.com>
2023-12-20 11:23:58 +0300

    Text replace mistake

--- src/apps/charts/components/Warning/index.tsx
+++ src/apps/charts/components/Warning/index.tsx
@@ -49,16 +49,16 @@ const Warning: FC<{
         represent projects that do not have a token.
       </Text>
       <Text fontWeight={500} lineHeight={"145.23%"} mt={"10px"}>
-        Similar to fathom-etherscan, this site automatically tracks analytics
-        for all ERC20 tokens independent of token integrity. Please do your own
-        research before interacting with any ERC20 token.
+        Similar to Etherscan, this site automatically tracks analytics for all
+        ERC20 tokens independent of token integrity. Please do your own research
+        before interacting with any ERC20 token.
       </Text>
     </div>
   ) : (
     <Text fontWeight={500} lineHeight={"145.23%"} mt={"10px"}>
       Anyone can create and name any ERC20 token on Ethereum, including creating
       fake versions of existing tokens and tokens that claim to represent
-      projects that do not have a token. Similar to fathom-etherscan, this site
+      projects that do not have a token. Similar to Etherscan, this site
       automatically tracks analytics for all ERC20 tokens independent of token
       integrity. Please do your own research before interacting with any ERC20
       token.
