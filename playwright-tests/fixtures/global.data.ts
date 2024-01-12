aa43799800d077ae5b5b99b16d0cfc195adc74d3
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2024-01-12 18:56:13 +0400

    Updated vaults tests according to latest changes

--- playwright-tests/fixtures/global.data.ts
+++ playwright-tests/fixtures/global.data.ts
@@ -1,4 +1,5 @@
 export const contractAddresses = {
-  fathomStablecoin: "0x321ec198d054e5f2b11183a2dfdd898e0ee64af4",
+  fathomStableCoin: "0xDf29cB40Cb92a1b8E8337F542E3846E185DefF96",
+  fathomStablecoinVaults: "0x321ec198d054e5f2b11183a2dfdd898e0ee64af4",
   testXdcToken: "0xcefC4215f4F92a80ab5F2b2A8E94078A3E79b26E",
 };
