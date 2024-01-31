27b6cc4f345953be52c04b0470e886acaf1eaa7d
babchenko <vetalion1@gmail.com>
2024-01-31 17:01:12 +0300

    Lending remove markets

--- src/apps/lending/ui-config/marketsConfig.tsx
+++ src/apps/lending/ui-config/marketsConfig.tsx
@@ -1,5 +1,4 @@
 import { ChainId } from "@into-the-fathom/lending-contract-helpers";
-import { AaveV3Ethereum, AaveV3Sepolia } from "@bgd-labs/aave-address-book";
 import { FathomLendingApothem } from "@into-the-fathom/fathom-lending-address-book";
 import { ReactNode } from "react";
 
@@ -50,9 +49,7 @@ export type MarketDataType = {
 export enum CustomMarket {
   // v3 test networks, all v3.0.1
   proto_apothem_v3 = "proto_apothem_v3",
-  proto_sepolia_v3 = "proto_sepolia_v3",
   // v3 mainnets
-  proto_mainnet_v3 = "proto_mainnet_v3",
 }
 
 export const marketsData: {
@@ -77,50 +74,6 @@ export const marketsData: {
       UI_INCENTIVE_DATA_PROVIDER:
         FathomLendingApothem.UI_INCENTIVE_DATA_PROVIDER,
       COLLECTOR: FathomLendingApothem.COLLECTOR,
-    },
-  },
-  [CustomMarket.proto_mainnet_v3]: {
-    marketTitle: "Ethereum",
-    chainId: ChainId.mainnet,
-    v3: true,
-    enabledFeatures: {
-      incentives: true,
-    },
-    subgraphUrl: "https://api.thegraph.com/subgraphs/name/aave/protocol-v3",
-    addresses: {
-      LENDING_POOL_ADDRESS_PROVIDER: AaveV3Ethereum.POOL_ADDRESSES_PROVIDER,
-      LENDING_POOL: AaveV3Ethereum.POOL,
-      WETH_GATEWAY: AaveV3Ethereum.WETH_GATEWAY,
-      REPAY_WITH_COLLATERAL_ADAPTER:
-        AaveV3Ethereum.REPAY_WITH_COLLATERAL_ADAPTER,
-      SWAP_COLLATERAL_ADAPTER: AaveV3Ethereum.SWAP_COLLATERAL_ADAPTER,
-      WALLET_BALANCE_PROVIDER: AaveV3Ethereum.WALLET_BALANCE_PROVIDER,
-      UI_POOL_DATA_PROVIDER: AaveV3Ethereum.UI_POOL_DATA_PROVIDER,
-      UI_INCENTIVE_DATA_PROVIDER: AaveV3Ethereum.UI_INCENTIVE_DATA_PROVIDER,
-      COLLECTOR: AaveV3Ethereum.COLLECTOR,
-      WITHDRAW_SWITCH_ADAPTER: AaveV3Ethereum.WITHDRAW_SWAP_ADAPTER,
-      DEBT_SWITCH_ADAPTER: AaveV3Ethereum.DEBT_SWAP_ADAPTER,
-    },
-    halIntegration: {
-      URL: "https://app.hal.xyz/recipes/aave-v3-track-health-factor",
-      marketName: "aavev3",
-    },
-  },
-  [CustomMarket.proto_sepolia_v3]: {
-    marketTitle: "Ethereum Sepolia",
-    v3: true,
-    chainId: ChainId.sepolia,
-    enabledFeatures: {
-      faucet: true,
-    },
-    addresses: {
-      LENDING_POOL_ADDRESS_PROVIDER: AaveV3Sepolia.POOL_ADDRESSES_PROVIDER,
-      LENDING_POOL: AaveV3Sepolia.POOL,
-      WETH_GATEWAY: AaveV3Sepolia.WETH_GATEWAY,
-      FAUCET: AaveV3Sepolia.FAUCET,
-      WALLET_BALANCE_PROVIDER: AaveV3Sepolia.WALLET_BALANCE_PROVIDER,
-      UI_POOL_DATA_PROVIDER: AaveV3Sepolia.UI_POOL_DATA_PROVIDER,
-      UI_INCENTIVE_DATA_PROVIDER: AaveV3Sepolia.UI_INCENTIVE_DATA_PROVIDER,
     },
   },
 } as const;
