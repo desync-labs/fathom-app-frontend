9147556953fd953d1422c3b0cfc41ae2fe66b5c4
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2024-02-05 16:41:52 +0400

    Swap: Insufficient balance is correctly displayed and Wrap: Insufficient balance is correctly displayed tests

--- playwright-tests/tests/dex/swap.spec.ts
+++ playwright-tests/tests/dex/swap.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "../../fixtures/pomSynpressFixture";
+import { test, expect } from "../../fixtures/pomSynpressFixture";
 import { WalletConnectOptions } from "../../types";
 import dotenv from "dotenv";
 import { tokenIds } from "../../fixtures/dex.data";
@@ -43,7 +43,7 @@ test.describe("Fathom App Test Suite: Positions Operations", () => {
     await dexPage.navigate();
     await dexPage.connectWallet(WalletConnectOptions.Metamask);
     await dexPage.validateConnectedWalletAddress();
-    const expectedData = await dexPage.unwrap({
+    const expectedData = await dexPage.wrap({
       fromToken: tokenIds.WXDC,
       toToken: tokenIds.XDC,
       fromAmount: 0.5,
@@ -54,5 +54,42 @@ test.describe("Fathom App Test Suite: Positions Operations", () => {
       toAmountExpected: expectedData.toAmountExpected,
       toTokenNameExpected: expectedData.toTokenNameExpected,
     });
+  });
+
+  test("Swap: Insufficient balance is correctly displayed", async ({
+    dexPage,
+  }) => {
+    const tokenFrom = tokenIds.XDC;
+    await dexPage.navigate();
+    await dexPage.connectWallet(WalletConnectOptions.Metamask);
+    await dexPage.validateConnectedWalletAddress();
+    await dexPage.selectFromToken({ tokenId: tokenFrom });
+    await dexPage.selectToToken({ tokenId: tokenIds.xUSDT });
+    const fromBalance = await dexPage.getFromBalance();
+    await dexPage.fillFromValue({ inputValue: fromBalance + 1 });
+    await expect(dexPage.swapButton).toBeVisible();
+    await expect(dexPage.swapButton).toHaveText(
+      `Insufficient ${tokenFrom} balance`
+    );
+    await expect(dexPage.swapButton).toBeDisabled();
+  });
+
+  test("Wrap: Insufficient balance is correctly displayed", async ({
+    dexPage,
+  }) => {
+    const tokenFrom = tokenIds.XDC;
+    await dexPage.navigate();
+    await dexPage.connectWallet(WalletConnectOptions.Metamask);
+    await dexPage.validateConnectedWalletAddress();
+    await dexPage.selectFromToken({ tokenId: tokenFrom });
+    await dexPage.selectToToken({ tokenId: tokenIds.WXDC });
+    await dexPage.page.waitForTimeout(2000);
+    const fromBalance = await dexPage.getFromBalance();
+    await dexPage.fillFromValue({ inputValue: fromBalance + 1 });
+    await expect(dexPage.wrapButton).toBeVisible();
+    await expect(dexPage.wrapButton).toHaveText(
+      `Insufficient ${tokenFrom} balance`
+    );
+    await expect(dexPage.wrapButton).toBeDisabled();
   });
 });
