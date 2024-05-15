8cf4484343b1dfc79476bfebea79b47b71451166
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2024-05-15 16:40:06 +0400

    Added createStakeFTHMPosition method and other interaction methods for Staking 150 FTHM with a 7-day lock period is successful test

--- playwright-tests/pages/dao.page.ts
+++ playwright-tests/pages/dao.page.ts
@@ -2,6 +2,8 @@ import { type Page, type Locator, expect } from "@playwright/test";
 import BasePage from "./base.page";
 // @ts-ignore
 import * as metamask from "@synthetixio/synpress/commands/metamask";
+import { graphAPIEndpoints } from "../fixtures/api.data";
+import { GraphOperationName } from "../types";
 
 export default class DaoPage extends BasePage {
   readonly stakingPath: string;
@@ -12,6 +14,7 @@ export default class DaoPage extends BasePage {
   readonly myWalletBalanceFTHM: Locator;
   readonly myWalletBalanceFXD: Locator;
   readonly myWalletBalanceXDC: Locator;
+  readonly progressBar: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -27,6 +30,7 @@ export default class DaoPage extends BasePage {
     this.myWalletBalanceFTHM = this.page.getByTestId("dao-FTHM-balance");
     this.myWalletBalanceFXD = this.page.getByTestId("dao-FXD-balance");
     this.myWalletBalanceXDC = this.page.getByTestId("dao-XDC-balance");
+    this.progressBar = this.page.locator('[role="progressbar"]');
   }
 
   async navigateStaking(): Promise<void> {
@@ -35,5 +39,55 @@ export default class DaoPage extends BasePage {
 
   async navigateGovernance(): Promise<void> {
     await super.navigate(this.governancePath);
+  }
+
+  async enterStakingAmount({
+    stakingAmount,
+  }: {
+    stakingAmount: number;
+  }): Promise<void> {
+    await this.inputStakingAmount.clear();
+    await this.inputStakingAmount.fill(stakingAmount.toString());
+  }
+
+  async enterLockPeriod({ lockPeriod }: { lockPeriod: number }): Promise<void> {
+    await this.inputLockPeriod.clear();
+    await this.inputLockPeriod.fill(lockPeriod.toString());
+  }
+
+  async createStakeFTHMPosition({
+    stakingAmount,
+    lockPeriod,
+  }: {
+    stakingAmount: number;
+    lockPeriod: number;
+  }): Promise<string> {
+    await this.enterStakingAmount({ stakingAmount });
+    await this.enterLockPeriod({ lockPeriod });
+    await this.btnStake.click();
+    await expect.soft(this.progressBar).toBeVisible();
+    await this.page.waitForTimeout(1000);
+    await expect(this.divAlert).toBeHidden({ timeout: 100 });
+    await metamask.confirmTransaction();
+    const results = await Promise.all([
+      this.validateAlertMessage({
+        status: "pending",
+        title: "Creating Position Pending.",
+        body: "Click on transaction to view on Block Explorer.",
+      }),
+      this.validateAlertMessage({
+        status: "success",
+        title: "Lock position created successfully!",
+      }),
+      this.waitForGraphRequestByOperationName(
+        graphAPIEndpoints.daoSubgraph,
+        GraphOperationName.Stakers
+      ),
+    ]);
+    const response = await results[2].response();
+    const responseBody = await response?.json();
+    const newPositionLockId =
+      responseBody.data.stakers[0].lockPositions[0].lockId;
+    return newPositionLockId;
   }
 }
--- playwright-tests/tests/dao/staking.spec.ts
+++ playwright-tests/tests/dao/staking.spec.ts
@@ -16,7 +16,11 @@ test.describe("Fathom App Test Suite: DAO Staking", () => {
     test("Staking 150 FTHM with a 7-day lock period is successful", async ({
       daoPage,
     }) => {
-      await daoPage.navigateStaking();
+      const newPositionLockId = await daoPage.createStakeFTHMPosition({
+        stakingAmount: 1,
+        lockPeriod: 7,
+      });
+      console.log(newPositionLockId);
     });
 
     test("Early unstaking a FTHM staked position is successful", async ({
