bbde1a675c631de719a7331a31cad44b29e80058
Oleksandr Dovgopoliy <alexdovgopoly@gmail.com>
2024-05-16 16:31:10 +0300

    feat: add CGO pool

--- src/hooks/useOpenPosition.ts
+++ src/hooks/useOpenPosition.ts
@@ -303,12 +303,22 @@ const useOpenPosition = (
       const { collateral, fathomToken } = values;
 
       try {
-        const blockNumber = await positionService.openPosition(
-          account,
-          pool,
-          collateral,
-          fathomToken
-        );
+        let blockNumber;
+        if (pool.poolName.toUpperCase() === "XDC") {
+          blockNumber = await positionService.openPosition(
+            account,
+            pool,
+            collateral,
+            fathomToken
+          );
+        } else {
+          blockNumber = await positionService.openPositionERC20(
+            account,
+            pool,
+            collateral,
+            fathomToken
+          );
+        }
 
         setLastTransactionBlock(blockNumber as number);
         onClose();
@@ -333,6 +343,7 @@ const useOpenPosition = (
       await positionService.approve(account, collateralTokenAddress as string);
       setApproveBtn(false);
     } catch (e) {
+      console.error(e);
       setApproveBtn(true);
     }
 
