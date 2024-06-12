963119a2004f2bbc8778475a610920d3887d1472
babchenko <vetalion1@gmail.com>
2024-06-12 15:10:41 +0300

    feat: add kyc notification for vaults

--- src/components/Vault/AllVaultView.tsx
+++ src/components/Vault/AllVaultView.tsx
@@ -1,8 +1,10 @@
 import { FC, memo, MouseEvent } from "react";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import { PageHeader } from "components/Dashboard/PageHeader";
 import VaultList from "components/Vault/VaultList";
 import useSharedContext from "context/shared";
+import { InfoIcon } from "../Governance/Propose";
+import { WarningBox } from "../AppComponents/AppBox/AppBox";
 
 type AllVaultViewPropsType = {
   isMobileFiltersOpen: boolean;
@@ -28,6 +30,27 @@ const AllVaultView: FC<AllVaultViewPropsType> = ({
           title={"Vaults"}
           description={`Explore existing Vaults, and deposit your assets for a sustainable yield.`}
         />
+        <Grid item xs={12}>
+          <WarningBox sx={{ my: 3 }}>
+            <InfoIcon
+              sx={{ width: "16px", color: "#F5953D", height: "16px" }}
+            />
+            <Typography>
+              The new vault, created in partnership with TradeFlow, will launch
+              soon this month. <br />
+              Ensure you're eligible to participate by completing KYC at{" "}
+              <a
+                href={"https://kyc.tradeflow.network/"}
+                target={"_blank"}
+                rel={"noreferrer"}
+              >
+                https://kyc.tradeflow.network/
+              </a>
+              . Only KYC-verified users can deposit. <br />
+              Note: Participation and deposit amounts are limited.
+            </Typography>
+          </WarningBox>
+        </Grid>
         <Grid item xs={12} sx={{ marginTop: isMobile ? "5px" : "30px" }}>
           <VaultList
             isMobileFiltersOpen={isMobileFiltersOpen}
