60552e12437c3ba9146b4f458e6b33c67b4e4e9b
babchenko <vetalion1@gmail.com>
2024-06-25 19:46:01 +0300

    fix: apy config

---
+++ src/utils/Vaults/getApyConfig.ts
@@ -0,0 +1,8 @@
+export const ApyConfig = {
+  ["0xbf4adcc0a8f2c7e29f934314ce60cf5de38bfe8f".toLowerCase()]: 10,
+  ["0x2D8A913F47B905C71F0A3d387de863F3a1Cc8d78".toLowerCase()]: 10,
+  ["0xA6272625f8fCd6FC3b53A167E471b7D55095a40b".toLowerCase()]: 10,
+  ["0xfa6ed4d32110e1c27c9d8c2930e217746cb8acab".toLowerCase()]: 10,
+  ["0xb8e027e707da68c98919686d5edf9adbf2746ed9".toLowerCase()]: 10,
+  ["0x8134c61A86775CF688d3d321E5cd32787c4c7f88".toLowerCase()]: 10,
+} as const;
