cbe1cf6bbcf79940e869b8978b5df767c17804d3
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2024-07-11 13:33:00 +0300

    Test automation: update stablecoin endpoint

--- playwright-tests/fixtures/api.data.ts
+++ playwright-tests/fixtures/api.data.ts
@@ -1,5 +1,5 @@
 export const graphAPIEndpoints = {
-  stablecoinSubgraph: "/subgraphs/name/stablecoin-subgraph",
+  stablecoinSubgraph: "/subgraphs/name/stablecoin-subgraph-v2",
   vaultsSubgraph: "/subgraphs/name/vaults-subgraph",
   daoSubgraph: "/subgraphs/name/dao-subgraph",
 };
