4c25af968493e6018624b062a30e2201f270ddbc
Oleksandr Dovgopoliy <alexdovgopoly@gmail.com>
2024-07-18 15:23:53 +0300

    feat: redesign sidebar bottom menu

--- src/components/Dashboard/BottomLinks.tsx
+++ src/components/Dashboard/BottomLinks.tsx
@@ -2,21 +2,28 @@ import { FC } from "react";
 import { styled } from "@mui/material/styles";
 import { Box } from "@mui/material";
 
+import LinkedInSrc from "assets/svg/socials/linkedln.svg";
+import TelegramSrc from "assets/svg/socials/telegram.svg";
+import TwitterSrc from "assets/svg/socials/twitter.svg";
+
+const BottomMenu = styled(Box)`
+  position: absolute;
+  bottom: 25px;
+`;
+
 const LinksWrapper = styled(Box)`
   display: flex;
   flex-direction: column;
-  gap: 7px;
-  padding-left: 14px;
-  position: absolute;
-  bottom: 25px;
+  gap: 0;
 
   a {
-    font-size: 0.825rem;
-    font-weight: 500;
-    color: ${({ theme }) => theme.palette.text.primary};
+    font-size: 14px;
+    font-weight: 600;
+    color: #b7c8e5;
     opacity: 0.8;
     display: flex;
     justify-content: start;
+    padding: 8px 16px;
 
     &:hover {
       opacity: 1;
@@ -24,61 +31,76 @@ const LinksWrapper = styled(Box)`
   }
 `;
 
+const SocialLinksWrapper = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  padding: 8px 16px;
+
+  & a {
+    height: 24px;
+  }
+`;
+
 const BottomLinks: FC = () => {
   return (
-    <LinksWrapper>
-      <a href={"https://fathom.fi/"} rel="noreferrer" target={"_blank"}>
-        Fathom.fi
-      </a>
-      <a href={"https://docs.fathom.fi"} rel="noreferrer" target={"_blank"}>
-        Docs
-      </a>
-      <a href={"https://t.me/fathom_fi"} rel="noreferrer" target={"_blank"}>
-        Telegram
-      </a>
-      <a
-        href={"https://twitter.com/Fathom_fi"}
-        rel="noreferrer"
-        target={"_blank"}
-      >
-        Twitter
-      </a>
-      <a
-        href={"https://www.linkedin.com/company/fathom-protocol/"}
-        rel="noreferrer"
-        target={"_blank"}
-      >
-        LinkedIn
-      </a>
-      <a
-        href={"https://docs.fathom.fi/privacy-policy"}
-        rel="noreferrer"
-        target={"_blank"}
-      >
-        Privacy Policy
-      </a>
-      <a
-        href={"https://docs.fathom.fi/terms-of-service"}
-        rel="noreferrer"
-        target={"_blank"}
-      >
-        Terms of Service
-      </a>
-      <a
-        href={"https://docs.fathom.fi/fxd-deployments"}
-        target={"_blank"}
-        rel="noreferrer"
-      >
-        FXD
-      </a>
-      <a
-        href={"https://docs.fathom.fi/fthm-deployments"}
-        target={"_blank"}
-        rel="noreferrer"
-      >
-        FTHM
-      </a>
-    </LinksWrapper>
+    <BottomMenu>
+      <LinksWrapper>
+        <a href={"https://fathom.fi/"} rel="noreferrer" target={"_blank"}>
+          Fathom.fi
+        </a>
+        <a href={"https://docs.fathom.fi"} rel="noreferrer" target={"_blank"}>
+          Docs
+        </a>
+        <a
+          href={"https://docs.fathom.fi/privacy-policy"}
+          rel="noreferrer"
+          target={"_blank"}
+        >
+          Privacy Policy
+        </a>
+        <a
+          href={"https://docs.fathom.fi/terms-of-service"}
+          rel="noreferrer"
+          target={"_blank"}
+        >
+          Terms of Service
+        </a>
+        <a
+          href={"https://docs.fathom.fi/fxd-deployments"}
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          FXD
+        </a>
+        <a
+          href={"https://docs.fathom.fi/fthm-deployments"}
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          FTHM
+        </a>
+      </LinksWrapper>
+      <SocialLinksWrapper>
+        <a href={"https://t.me/fathom_fi"} rel="noreferrer" target={"_blank"}>
+          <img src={TelegramSrc} alt={"telegram"} />
+        </a>
+        <a
+          href={"https://twitter.com/Fathom_fi"}
+          rel="noreferrer"
+          target={"_blank"}
+        >
+          <img src={TwitterSrc} alt={"twitter"} />
+        </a>
+        <a
+          href={"https://www.linkedin.com/company/fathom-protocol/"}
+          rel="noreferrer"
+          target={"_blank"}
+        >
+          <img src={LinkedInSrc} alt={"linked-in"} />
+        </a>
+      </SocialLinksWrapper>
+    </BottomMenu>
   );
 };
 
