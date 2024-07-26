bf5658bdfd7f747f4a2486e28bbb99d4e8637f7c
babchenko <vetalion1@gmail.com>
2024-07-26 18:20:16 +0300

    fix: history page for lending

--- src/apps/lending/modules/history/HistoryFilterMenu.tsx
+++ src/apps/lending/modules/history/HistoryFilterMenu.tsx
@@ -17,6 +17,7 @@ import { FilterOptions } from "./types";
 
 import { Check as CheckIcon, Sort as SortIcon } from "@mui/icons-material";
 import CancelIcon from "@mui/icons-material/Cancel";
+import useSharedContext from "context/shared";
 
 interface HistoryFilterMenuProps {
   onFilterChange: (filter: FilterOptions[]) => void;
@@ -54,6 +55,7 @@ export const HistoryFilterMenu: React.FC<HistoryFilterMenuProps> = ({
   const [localFilter, setLocalFilter] =
     useState<FilterOptions[]>(currentFilter);
   const trackEvent = useRootStore((store) => store.trackEvent);
+  const { isMobile } = useSharedContext();
 
   useEffect(() => {
     onFilterChange(localFilter);
@@ -132,7 +134,7 @@ export const HistoryFilterMenu: React.FC<HistoryFilterMenuProps> = ({
   };
 
   return (
-    <Box sx={{ marginBottom: "30px" }}>
+    <Box sx={{ marginBottom: isMobile ? "30px" : "0" }}>
       <Button
         sx={{
           minWidth: 148,
