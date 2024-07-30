281b0d9b0a6ac1537b497f12a94bc4610d9dbca5
Oleksandr Dovgopoliy <alexdovgopoly@gmail.com>
2024-07-30 14:37:52 +0300

    fix: popover style

--- src/components/Base/Popover/BasePopover.tsx
+++ src/components/Base/Popover/BasePopover.tsx
@@ -9,23 +9,22 @@ const PopoverWrapper = styled(Popover)<{ type: PopoverType }>`
       type === PopoverType.Error ? "#FF6767" : "rgba(255, 255, 255, 0.9)"};
     color: #000c24;
     max-width: 400px;
-    border-radius: 12px;
-    padding: 12px 16px;
+    border-radius: 8px;
+    padding: 8px 16px;
 
     p {
-      font-size: 14px;
+      font-size: 12px;
       font-weight: 400;
-      line-height: 20px;
+      line-height: 16px;
     }
   }
 
   ${({ theme }) => theme.breakpoints.down("sm")} {
     .MuiPaper-root {
       max-width: 350px;
-      border-radius: 8px;
       padding: 8px 12px;
       p {
-        font-size: 12px;
+        font-size: 11px;
         line-height: 15px;
       }
     }
